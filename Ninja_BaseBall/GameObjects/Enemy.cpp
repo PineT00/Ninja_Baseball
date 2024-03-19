4156158d313a8fd390b1ef3d9d314f2dd8efefbc
방민호 <100828741+bamin0502@users.noreply.github.com>
2024-03-19 15:00:16 +0900

    몬스터 스크립트랑 몬스터 스폰 관련 스크립트 테스트 중

---
+++ Ninja_BaseBall/GameObjects/Enemy.cpp
@@ -0,0 +1,90 @@
+﻿#include "pch.h"
+#include "Enemy.h"
+
+Enemy* Enemy::Create(EnemyType enemyType)
+{
+    Enemy* enemy = new Enemy("Enemy");
+    enemy->type = enemyType;
+    switch (enemyType)
+    {
+    case EnemyType::BASEBALL_YELLOW:
+        enemy->textureId = "graphics/baseball_yellow.png";
+        enemy->monsterMaxHp = 100;
+        enemy->monsterHp = enemy->monsterMaxHp;
+        enemy->monsterSpeed = 100;
+        enemy->monsterDamage = 10;
+        enemy->monsterAttackRange = 50;
+        enemy->monsterAttackSpeed = 1.f;
+        enemy->animator.Play("BaseballYellow_Idle");
+        enemy->attackType = EnemyAttackType::SINGLE;
+        break;
+    case EnemyType::BASEBALL_GREEN:
+        
+        break;
+    case EnemyType::BASEBALL_BLUE:
+        
+        break;
+    case EnemyType::BASEBALL_WHITE:
+        
+        break;
+    case EnemyType::ELITE:
+        
+        break;
+    case EnemyType::BOSS:
+        
+        break;
+        case 
+    default: ;
+    }
+    return enemy;
+}
+
+Enemy::Enemy(const std::string& name): SpriteGo(name), state(EnemyState::IDLE), attackCoolTime(0), monsterHp(0), monsterMaxHp(0),
+                monsterSpeed(0),
+                monsterDamage(0),
+                monsterAttackRange(0),
+                monsterAttackSpeed(0)
+{
+}
+
+Enemy::~Enemy()
+= default;
+
+void Enemy::Init()
+{
+    SpriteGo::Init();
+    SetOrigin(Origins::MC);
+}
+
+void Enemy::Release()
+{
+    SpriteGo::Release();
+}
+
+void Enemy::Reset()
+{
+    SpriteGo::Reset();
+    monsterHp = monsterMaxHp;
+    
+}
+
+void Enemy::Update(float dt)
+{
+    SpriteGo::Update(dt);
+    
+}
+
+void Enemy::LateUpdate(float dt)
+{
+    SpriteGo::LateUpdate(dt);
+}
+
+void Enemy::FixedUpdate(float dt)
+{
+    SpriteGo::FixedUpdate(dt);
+}
+
+void Enemy::Draw(sf::RenderWindow& window)
+{
+    SpriteGo::Draw(window);
+}
---
+++ Ninja_BaseBall/GameObjects/Enemy.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+#include "Animator.h"
+#include "SpriteGo.h"
+
+class Enemy:public SpriteGo
+{
+private:
+    
+protected:
+    //Enemy Type
+    enum class EnemyType
+    {
+        NONE=-1,
+        BASEBALL_YELLOW,
+        BASEBALL_GREEN,
+        BASEBALL_BLUE,
+        BASEBALL_WHITE,
+        ELITE,
+        BOSS
+    };
+    EnemyType type=EnemyType::NONE;
+    
+    //Enemy State
+    enum class EnemyState
+    {
+        IDLE,
+        MOVE,
+        ATTACK,
+        DEAD
+    };
+    EnemyState state;
+
+    static Enemy* Create(EnemyType enemyType);
+    
+    //Enemy Attack Type
+    enum class EnemyAttackType
+    {
+        SINGLE,
+        COMBO,
+        CHARGE
+    };
+    EnemyAttackType attackType;
+    Animator animator;
+    
+public:
+    Enemy(const std::string& name);
+    
+    virtual ~Enemy();
+    void Init() override;
+    void Release() override;
+    void Reset() override;
+    void Update(float dt) override;
+    void LateUpdate(float dt) override;
+    void FixedUpdate(float dt) override;
+    void Draw(sf::RenderWindow& window) override;
+
+protected:
+    float attackCoolTime;
+    float monsterHp;
+    float monsterMaxHp;
+    float monsterSpeed;
+    float monsterDamage;
+    float monsterAttackRange;
+    float monsterAttackSpeed;
+};
