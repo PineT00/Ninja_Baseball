690c29a3c7a4d8505b380762aab5d73adb9b7348
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-03-24 13:57:33 +0900

    보스 데이터 테이블 Json으로 변경
    보스 데이터 테이블 구조
    Boss
      WindyPlane
        files
          "animClip"

--- Ninja_BaseBall/BossTable.h
+++ Ninja_BaseBall/BossTable.h
@@ -1,38 +1,40 @@
 #pragma once
 #include "DataTable.h"
+#include "WindyPlane.h" // WindyPlane의 정의를 포함해야 합니다.
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/rapidjson.h"
+#include <rapidjson/istreamwrapper.h>
 
 struct BossData
 {
-	BossType id;
-	std::string nameId;
-	std::string animationChaseClipId;
-	std::string animationPunchOneClipId;
-	std::string animationPunchTwoClipId;
-	std::string animationWindAttackClipId;
-	std::string animationGunAttackClipId;
-	std::string animationCryingClipId;
-	std::string animationDeadClipId;
+    BossPartsStatus status;
+    std::vector<std::string> files;
+    BossData() {}
+    BossData(BossPartsStatus status, std::vector<std::string> files)
+        : status(status), files(std::move(files)) {}
 };
+
 
 class BossTable : public DataTable
 {
 public:
-	static BossData Undefined;
+    static std::unordered_map<BossPartsStatus, BossData> Undefined;
 
 protected:
-	BossTable(const BossTable&) = delete;
-	BossTable(BossTable&&) = delete;
-	BossTable& operator=(const BossTable&) = delete;
-	BossTable& operator=(BossTable&&) = delete;
+    BossTable(const BossTable&) = delete;
+    BossTable(BossTable&&) = delete;
+    BossTable& operator=(const BossTable&) = delete;
+    BossTable& operator=(BossTable&&) = delete;
 
-	std::unordered_map <BossType, BossData> table;
+    std::unordered_map<std::string, std::unordered_map<BossPartsStatus, BossData>> table;
 
 public:
-	BossTable(Types type);
-	~BossTable() override = default;
+    BossTable(Types type);
+    ~BossTable() override = default;
 
-	bool Load() override;
-	void Release() override;
+    bool Load() override;
+    void Release() override;
 
-	const BossData Get(BossType id);
+    std::unordered_map<BossPartsStatus, BossData>& Get(BossType id);
 };
