8944088b8113f864821fb67b6909054a758e052b
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-03-28 17:51:14 +0900

    보스 피격, 잡힌 후 상태 타이머 공격 가능 상태로 초기화

--- Ninja_BaseBall/main.cpp
+++ Ninja_BaseBall/main.cpp
@@ -4,7 +4,6 @@ int main()
 {
     //FRAMEWORK.Init(1280, 960, "Ninja Baseball");
     FRAMEWORK.Init(960, 720, "Ninja Baseball");
-    
     //FRAMEWORK.Init(1920, 1080, "Ninja Baseball");
     FRAMEWORK.Do();
     FRAMEWORK.Release();
