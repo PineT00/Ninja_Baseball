e147da37da605adc9b56a41852fdb6baaec10e9e
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-03-29 12:06:59 +0900

    게임 시작 시 큰 리소스 로드

--- Ninja_BaseBall/Framework/SceneManager.h
+++ Ninja_BaseBall/Framework/SceneManager.h
@@ -31,7 +31,11 @@ protected:
 	SceneIDs currentScene = startScene;
 	SceneIDs nextScene = SceneIDs::None;
 
-	std::vector<std::string> TextureResourceNames = {};
+	std::vector<std::string> TextureResourceNames = {"graphics/4_Boss/boss_attacked(no_arm).bmp", "graphics/4_Boss/boss_attacked(no_one_arm).bmp", "graphics/4_Boss/boss_attacked(no_wing).bmp", "graphics/4_Boss/boss_attacked(no_wing_prop).bmp",
+	"graphics/4_Boss/boss_attacked.bmp", "graphics/4_Boss/boss_gun_ready(no_wing).bmp", "graphics/4_Boss/boss_idle(basic).bmp", "graphics/4_Boss/boss_idle(no_one_arm).bmp", "graphics/4_Boss/boss_idle(no_wing).bmp",  
+		"graphics/4_Boss/boss_death.bmp", "graphics/4_Boss/boss_final.bmp", "graphics/4_Boss/boss_gun(no_wing).bmp", "graphics/4_Boss/boss_gun_prop(no_wing).bmp", "graphics/4_Boss/boss_idle(no_wing_prop).bmp", "graphics/4_Boss/boss_onetwo.bmp", "graphics/4_Boss/boss_straight(no_one_arm).bmp" 
+	, "graphics/4_Boss/boss_straight(no_wing).bmp" , "graphics/4_Boss/boss_straight(no_wing_prop).bmp" , "graphics/4_Boss/boss_straight.bmp" , "graphics/4_Boss/boss_straight2.bmp" , "graphics/4_Boss/boss_upper_cut(no_wing).bmp" , "graphics/4_Boss/boss_upper_cut(no_wing_prop).bmp",
+	"graphics/4_Boss/boss_upper_cut.bmp", "graphics/4_Boss/boss_wind.bmp", "graphics/4_Boss/boss_windEffect.bmp", "graphics/4_Boss/sboss_onetwo.bmp", "graphics/4_Boss/tboss_onetwo.bmp"};
 	std::vector<std::string> FontResourceNames = {};
 	std::vector<std::string> SoundResourceNames = {};
 
